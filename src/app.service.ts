4ed5cc15e78059650c9409d91b5fa396e106cd4b
abdulsamad <asamad@elixir.com>
2023-07-10 12:50:13 +0500

    updated message in app

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return ('\t\t\tWelcome to Twilio service\nPlease call the twilio number +1 484-729-9629 to test the application');
+    return 'tWelcome to Twilio service. Please call the twilio number +1 484-729-9629 to test the application';
   }
 }
